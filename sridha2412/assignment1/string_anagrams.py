64a16a654c332fa7cde4710b6e1704e48be3cee9
Sridha Agarwal <2208414a@student.gla.ac.uk>
2018-05-20 17:11:52 +0100

    q1 completed with words and sent

--- sridha2412/assignment1/string_anagrams.py
+++ sridha2412/assignment1/string_anagrams.py
@@ -1,7 +1,6 @@
 # python program to determine if two strings are anagrams of each other
 
 # assumption: only using the 26 alphabets of the english language
-
 # method to check if two strings are anagrams
 # method takes two strings and a boolean value. If true, check case sensitive anagrams else ignore case
 def isAnagram(str1, str2, caseSensitive):
@@ -12,13 +11,38 @@ def isAnagram(str1, str2, caseSensitive):
     # sort and compare the strings
     return sorted(str1) == sorted(str2)
 
+# assumption: sentence can only include alphabets and spaces
+# method to check if two sentences are anagrams
+# method takes two sentences and a boolean value. If true, check case sensitive anagrams else ignore case
+def isSentenceAnagram(str1, str2, caseSensitive):
+    # if ignoring case, convert both strings to lower case
+    if not caseSensitive:
+        str1 = str1.lower()
+        str2 = str2.lower()
+
+    # split sentence into words
+    str1 = str1.split(' ')
+    str2 = str2.split(' ')
+
+    # sort each word and add it to list
+    for i, word in enumerate(str1):
+        str1[i] = sorted(list(word))
+    for i, word in enumerate(str2):
+        str2[i] = sorted(list(word))
+
+    # sort and compare the strings
+    return sorted(str1) == sorted(str2)
+
 str1 = input("Enter first string: ")
 str2 = input("Enter second string: ")
-caseSensitive = input("Enter Y if check case sensitive anagram else N: ")
+caseSensitive = input("Enter Y if case sensitive else N: ")
 # default value is False
 caseSensitive = True if caseSensitive == 'Y' else False
 
-if isAnagram(str1, str2, caseSensitive):
+# for words
+# if isAnagram(str1, str2, caseSensitive):
+# for sentences
+if isSentenceAnagram(str1, str2, caseSensitive):
     print("\""+str1+"\" and \""+str2+"\" are anagrams")
 else:
     print("\""+str1+"\" and \""+str2+"\" are NOT anagrams")
