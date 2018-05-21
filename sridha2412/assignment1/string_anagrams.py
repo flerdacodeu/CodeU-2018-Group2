80ccb47d29125aec11b48cdfdf6796bdce016e62
Sridha2412 <2208414a@student.gla.ac.uk>
2018-05-21 21:15:13 +0100

    made changes according to comments

--- sridha2412/assignment1/string_anagrams.py
+++ sridha2412/assignment1/string_anagrams.py
@@ -3,7 +3,7 @@
 # assumption: only using the 26 alphabets of the english language
 # method to check if two strings are anagrams
 # method takes two strings and a boolean value. If true, check case sensitive anagrams else ignore case
-def isAnagram(str1, str2, caseSensitive):
+def is_anagram(str1, str2, caseSensitive):
     # if ignoring case, convert both strings to lower case
     if not caseSensitive:
         str1 = str1.lower()
@@ -14,24 +14,25 @@ def isAnagram(str1, str2, caseSensitive):
 # assumption: sentence can only include alphabets and spaces
 # method to check if two sentences are anagrams
 # method takes two sentences and a boolean value. If true, check case sensitive anagrams else ignore case
-def isSentenceAnagram(str1, str2, caseSensitive):
-    # if ignoring case, convert both strings to lower case
-    if not caseSensitive:
-        str1 = str1.lower()
-        str2 = str2.lower()
-
+def is_sentence_anagram(str1, str2, caseSensitive):
     # split sentence into words
     str1 = str1.split(' ')
     str2 = str2.split(' ')
-
-    # sort each word and add it to list
-    for i, word in enumerate(str1):
-        str1[i] = sorted(list(word))
-    for i, word in enumerate(str2):
-        str2[i] = sorted(list(word))
-
-    # sort and compare the strings
-    return sorted(str1) == sorted(str2)
+    # check both have equal number of words
+    if len(str1) != len(str2):
+        return False
+    # iterate over both lists
+    for i in range(0, len(str1)):
+        # check if an anagram exists in second list
+        for j in range(0, len(str2)):
+            if is_anagram(str1[i], str2[j], caseSensitive):
+                # if yes, delete item from list and check for next element
+                del(str2[j])
+                break
+        # check anagram has been found
+        if len(str2) != len(str1) - i - 1:
+            return False
+    return True
 
 str1 = input("Enter first string: ")
 str2 = input("Enter second string: ")
@@ -39,10 +40,5 @@ caseSensitive = input("Enter Y if case sensitive else N: ")
 # default value is False
 caseSensitive = True if caseSensitive == 'Y' else False
 
-# for words
-# if isAnagram(str1, str2, caseSensitive):
-# for sentences
-if isSentenceAnagram(str1, str2, caseSensitive):
-    print("\""+str1+"\" and \""+str2+"\" are anagrams")
-else:
-    print("\""+str1+"\" and \""+str2+"\" are NOT anagrams")
+is_it_or_not = 'NOT ' if not is_sentence_anagram(str1, str2, caseSensitive) else ''
+print('"' + str1 + '" and "' + str2 + '" are ' + is_it_or_not + 'anagrams')
