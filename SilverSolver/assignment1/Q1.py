4907b40b465f9528af72912912706860cb34547d
Laida <K.Lidia@list.ru>
2018-05-24 10:36:37 +0300

    shortcomings, which were mentioned in the discussions, are resolved now

--- SilverSolver/assignment1/Q1.py
+++ SilverSolver/assignment1/Q1.py
@@ -1,22 +1,15 @@
+from collections import Counter
+import re
+
 def are_words_anagram(s1, s2, case_sensitive=False, return_dicts=False):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
     if not case_sensitive:
         s1 = s1.lower()
         s2 = s2.lower()
-    else:
-        alphabet += alphabet.upper()
-    alphabet += "0123456789"
-    s1 = s1.strip()         # Ignoring spaces
-    s2 = s2.strip()
+    s1 = re.sub(r'[\W\s]', '', s1)
+    s2 = re.sub(r'[\W\s]', '', s2)
 
-    char_counter_1 = dict.fromkeys(list(alphabet), 0)
-    char_counter_2 = char_counter_1.copy()
-    for c in s1:
-        if c in char_counter_1.keys():
-            char_counter_1[c] += 1
-    for c in s2:
-        if c in char_counter_2.keys():
-            char_counter_2[c] += 1
+    char_counter_1 = Counter(s1)
+    char_counter_2 = Counter(s2)
 
     if return_dicts:
         return (char_counter_1, char_counter_2)
@@ -24,26 +17,25 @@ def are_words_anagram(s1, s2, case_sensitive=False, return_dicts=False):
         return char_counter_1 == char_counter_2
 
 def are_sentences_anagram(s1, s2, case_sensitive=False):
-    s1 = s1.strip()
-    s2 = s2.strip()
+    s1 = re.sub(r'[\W]', ' ', s1)
+    s2 = re.sub(r'[\W]', ' ', s2)
     words1 = s1.split()
     words2 = s2.split()
+
     if len(words1) != len(words2):
         return False
     else:
-        words_counter_1 = dict()
-        words_counter_2 = dict()
+        words_1 = []
+        words_2 = []
         for i in range(len(words1)):
             w1, w2 = are_words_anagram(words1[i], words2[i], \
                      case_sensitive=case_sensitive, return_dicts=True)
             w1 = frozenset(w1.items())
             w2 = frozenset(w2.items())
-            if w1 not in words_counter_1.keys():
-                words_counter_1[w1] = 1
-            else:
-                words_counter_1[w1] += 1
-            if w2 not in words_counter_2.keys():
-                words_counter_2[w2] = 1
-            else:
-                words_counter_2[w2] += 1
+            words_1.append(w1)
+            words_2.append(w2)
+
+        words_counter_1 = Counter(words_1)
+        words_counter_2 = Counter(words_2)
+
         return words_counter_1 == words_counter_2
--- SilverSolver/assignment1/Q2.py
+++ SilverSolver/assignment1/Q2.py
@@ -1,38 +1,44 @@
 class single_linked_node:
+
     def __init__(self, value=None, child=None):
         self.child = child
         self.value = value
 
-def return_k_th_from_end_element(k, start_node):
-    list_len = 0
-    current_node = start_node
-    while (current_node):
-        list_len += 1
-        current_node = current_node.child
-    element_num = list_len - k
-    if element_num < 0 or element_num > list_len:
-        print("wrong k")
-        return None
-    else:
-        current_num  = 0
-        current_node = start_node
-        while(current_node):
-            current_num += 1
-            if current_num == element_num:
-                return current_node.value
+    def __getitem__(self, k):
+        k = -k
+        list_len = 0
+        current_node = self
+        while (current_node):
+            list_len += 1
             current_node = current_node.child
+        element_num = list_len - k
+        if element_num < 0 or element_num > list_len:
+            raise IndexError('wrong k')
+        else:
+            current_num  = 0
+            current_node = self
+            while(current_node):
+                current_num += 1
+                if current_num == element_num:
+                    return current_node.value
+                current_node = current_node.child
+
+def from_iterable(iterable):
+    prev_node = single_linked_node(iterable[0], None)
+    for i in range(1, len(iterable)):
+        prev_node = single_linked_node(iterable[i], prev_node)
+    return prev_node
+
+def return_k_th_from_end_element(k, node):
+    return node[-k]
+
 
 if __name__ == "__main__":
     # Demonstration of the functional
-    test_list = [single_linked_node(0, None)]
-    for i in range(1, 10):
-        test_list.append(single_linked_node(i, test_list[-1]))
+
+    iterable = [i for i in range(10)]
+    example_list = from_iterable(iterable) 
     print("we generated list 0 <- 1 <- 2 <- 3 <- 4 <- 5 <- 6 <- 7 <- 8 <- 9 for example")
-
-    current_node = test_list[-1] 
-    while (current_node):
-        current_node = current_node.child
-
-    print("choose k for testing return_k_th_from_end_element function")
+    print("choose k for testing node[k] function")
     k = int(input())
-    print(return_k_th_from_end_element(k, test_list[-1]))
+    print(example_list[k])
