81bacf4c1afe57911487ba63d2a7aa4765e6a376
crossopt <s17b2_fedorkina@179.ru>
2018-05-25 10:42:55 +0300

    fixed issues, no unit tests

--- crossopt/assignment1/Q1.py
+++ crossopt/assignment1/Q1.py
@@ -1,40 +1,34 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from re import split
+from collections import Counter
 
 
-# countsorts elements in container, elements must be hashable
-def countSort(container):
-    count = dict()
-    for i in container:
-        count[i] = count.get(i, 0) + 1
-    return count
+# return hashable count of elements
+def hashCount(container):
+    return tuple(sorted(Counter(container).items()))
 
 
 # checks whether strings are anagrams
-def isStringAnagram(str1, str2, caseSensitive=False):
-    if not caseSensitive:
+def isStringAnagram(str1, str2, case_sensitive=False):
+    if not case_sensitive:
         str1 = str1.lower()
         str2 = str2.lower()
 
-    return countSort(str1) == countSort(str2)
+    return Counter(str1) == Counter(str2)
 
 
 # checks whether sentences are anagrams. Punctuation is ignored
-def isSentenceAnagram(str1, str2, caseSensitive=False):
-    if not caseSensitive:
+def isSentenceAnagram(str1, str2, case_sensitive=False):
+    if not case_sensitive:
         str1 = str1.lower()
         str2 = str2.lower()
 
-    punctuation = '[! "#$%&()*+,\./:;<=>?@[\\]^`{|}~]'
-    # get list of words in sentence
-    words1 = [word for word in split(punctuation, str1) if word]
-    # replace all words with their CountSort, make it hashable
-    words1 = [(tuple(sorted(countSort(word).items()))) for word in words1]
-    words2 = [word for word in split(punctuation, str2) if word]
-    words2 = [(tuple(sorted(countSort(word).items()))) for word in words2]
-
-    return countSort(words1) == countSort(words2)
+    # TODO - figure out apostrophes
+    # get list of words, replace all words with their count, make it hashable
+    words1 = [hashCount(word) for word in split('\W', str1) if word]
+    words2 = [hashCount(word) for word in split('\W', str2) if word]
+    return Counter(words1) == Counter(words2)
 
 
 if __name__ == "__main__":
