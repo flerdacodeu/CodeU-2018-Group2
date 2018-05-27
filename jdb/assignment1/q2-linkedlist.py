6348c95d3d07c8aa6199bdff32a8454e09908392
Jean-Daniel Browne <jeandaniel.browne@gmail.com>
2018-05-27 05:49:43 +0000

    Added complexity considerations and more information about iterators.

--- jdb/assignment1/q2-linkedlist.py
+++ jdb/assignment1/q2-linkedlist.py
@@ -2,29 +2,52 @@ class Node:
   def __init__(self, value=None):
     self.value, self.next = value, None
 
+# This implementation uses the iterator a lot, it is not better in
+# terms of complexity:
+# pros: shorter methods, more consistent with other Python containers list, dicts...
+# cons: confusing to the reader unfamiliar with iterators
+#
+# resource on Python's iterators:
+# https://www.programiz.com/python-programming/iterator
+# http://www.diveintopython3.net/iterators.html
 
 class LinkedList:
   def __init__(self, original_list):
+    # __init__ loops over the input list: O(n)
     current = self.head = Node(original_list[0])
     for value in original_list[1:]:
       current.next = Node(value)
       current = current.next
 
   def __iter__(self):
+    # - iter returns a interator object in constant time.
+    # - then the caller of the iterator can call 'next()'
+    #   and next will returns each Node, and once exhausted
+    #   the iterator will raise a StopIteration exception.
+    #
+    # When using the for loop over an iterable, __iter__
+    # then __next__ are called automatically (these steps
+    # are called the iterator protocol).
     current = self.head
     while current:
       yield current
       current = current.next
 
   def __len__(self):
+    # enumerate returns an iterator immediately, and the for
+    # loop will call next() on the the enumerate iterator until
+    # exhausted. You can call next on an enumerate iterator as
+    # many times as there are elements in the iterator provided
+    # to enumerate. __iter__ will yields the n elements, so enumerate
+    # will also return n elements. __len__ is in O(n).
     for index, _ in enumerate(self): pass
     return index + 1
 
   def __getitem__(self, k):
     if k < 0:
-      return self[len(self) + k]
+      return self[len(self) + k]  # O(n) due to len()
     else:
-      for index, node in enumerate(self):
+      for index, node in enumerate(self):  # O(n) due to __iter__
         if index == k:
           return node
       else:
