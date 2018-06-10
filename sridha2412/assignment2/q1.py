c579bf916115caebe3d2d253827fd505eeb32b33
Sridha2412 <2208414a@student.gla.ac.uk>
2018-06-10 12:01:23 +0100

    made changes according to comments

--- sridha2412/assignment2/q1.py
+++ sridha2412/assignment2/q1.py
@@ -2,28 +2,40 @@
 
 # node class`
 class Node:
-    # constuctor to initlalize a node object
-    def __init__(self, data):
+    def __init__(self, data: int):
+        """ constuctor to initlalize a node object """
         self.data = data
         self.left = None
         self.right = None
 
-# if key is present, print ancestors else return false
-def print_ancestors(root, key):
-    # base case
+def find_ancestors(root: Node, key: int, ancestors: list) -> bool:
+    """ if key is present, return list of ancestors else return false """
+    # base cases
     if root is None:
         return False
-
     if root.data == key:
         return True
 
+    # store all nodes in list
+    ancestors.append(root.data)
+
     # search for key in left and right subtrees
-    if print_ancestors(root.left, key) or print_ancestors(root.right, key) :
-        print (root.data, end = " ")
-        return True
+    if find_ancestors(root.left, key, ancestors) or find_ancestors(root.right, key, ancestors) :
+        return ancestors
+
+    # if not ancestor, delete from list
+    ancestors.pop()
 
     return False
 
+
+#       7
+#      / \
+#     3   4
+#    / \   \
+#   2   5   8
+#  / \
+# 1   6
 
 root = Node(7)
 root.left = Node(3)
@@ -33,4 +45,37 @@ root.left.right = Node(5)
 root.right.right = Node(8)
 root.left.left.left = Node(1)
 root.left.left.right = Node(6)
-print_ancestors(root, 10)
+ancestors = []
+print("Key not found" if not find_ancestors(root, 6, ancestors)
+else ", ".join([str(i) for i in ancestors]))
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        #       7
+        #      / \
+        #     3   4
+        #    / \   \
+        #   2   5   8
+        #  / \
+        # 1   6
+        self.root = Node(7)
+        self.root.left = Node(3)
+        self.root.right = Node(4)
+        self.root.left.left = Node(2)
+        self.root.left.right = Node(5)
+        self.root.right.right = Node(8)
+        self.root.left.left.left = Node(1)
+        self.root.left.left.right = Node(6)
+
+    def test_init(self):
+        self.assertEqual(self.root.data, 7)
+        self.assertEqual(self.root.right.data, 4)
+        self.assertEqual(self.root.left.data, 3)
+
+    def test_ancestors(self):
+        self.assertEqual(find_ancestors(self.root, 6, []), [7, 3, 2])
+        self.assertEqual(find_ancestors(self.root, 8, []), [7, 4])
+        self.assertEqual(find_ancestors(self.root, 10, []), False)
--- sridha2412/assignment2/q2.py
+++ sridha2412/assignment2/q2.py
@@ -2,32 +2,32 @@
 
 # node class`
 class Node:
-    # constuctor to initlalize a node object
-    def __init__(self, data):
+    def __init__(self, data: int):
+        """ constuctor to initlalize a node object """
         self.data = data
         self.left = None
         self.right = None
 
-# method to find node is present in tree
-def find_node(root, node):
+def is_present(root: Node, key: int) -> bool:
+    """ method to check if node is present in tree """
     if root is None:
         return False
 
-    if root.data == node or find_node(root.right, node) or find_node(root.left, node):
+    if root.data == key or is_present(root.right, key) or is_present(root.left, key):
         return True
 
     return False
 
-# method to find least common ancestor of two given nodes
-# note: if one key is ancestor of another, it returns the key
-def find_lca(root, node1, node2):
+def find_lca(root: Node, node1: int, node2: int) -> int:
+    """ method to find least common ancestor of two given nodes
+        note: if one key is ancestor of another, it returns the key """
     # base cases
     if root is None:
         return None
     if root.data == node1:
-        return root
+        return root.data
     if root.data == node2:
-        return root
+        return root.data
 
     # search for keys in left and right subtrees
     left_lca = find_lca(root.left, node1, node2)
@@ -35,11 +35,18 @@ def find_lca(root, node1, node2):
 
     # if both return non None values, one key is present in left subtree and the other in right
     if left_lca and right_lca:
-        return root
+        return root.data
 
     # else check right subtree if left is None and vice-versa
     return right_lca if left_lca is None else left_lca
 
+#       7
+#      / \
+#     3   4
+#    / \   \
+#   2   5   8
+#  / \
+# 1   6
 
 root = Node(7)
 root.left = Node(3)
@@ -54,7 +61,41 @@ node1 = 6
 node2 = 5
 
 # check both nodes are present in tree
-if not find_node(root, node1) or not find_node(root, node2):
+if not is_present(root, node1) or not is_present(root, node2):
     print ("Given nodes are not present")
 else:
-    print("LCA(", node1, ", ", node2, " = ", find_lca(root, node1, node2).data, ")", sep = "")
+    print("LCA(", node1, ", ", node2, " = ", find_lca(root, node1, node2), ")", sep = "")
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        #       7
+        #      / \
+        #     3   4
+        #    / \   \
+        #   2   5   8
+        #  / \
+        # 1   6
+        self.root = Node(7)
+        self.root.left = Node(3)
+        self.root.right = Node(4)
+        self.root.left.left = Node(2)
+        self.root.left.right = Node(5)
+        self.root.right.right = Node(8)
+        self.root.left.left.left = Node(1)
+        self.root.left.left.right = Node(6)
+
+    def test_init(self):
+        self.assertEqual(self.root.data, 7)
+        self.assertEqual(self.root.right.data, 4)
+        self.assertEqual(self.root.left.data, 3)
+
+    def test_is_present(self):
+        self.assertEqual(is_present(self.root, 1), True)
+        self.assertEqual(is_present(self.root, 10), False)
+
+    def test_lca(self):
+        self.assertEqual(find_lca(self.root, 5, 6), 3)
+        self.assertEqual(find_lca(self.root, 3, 2), 3)
