b206792bdf9d4fc3eef909af2c7f9097f9b8e587
Laida <K.Lidia@list.ru>
2018-07-03 09:13:57 +0300

    changes were made according the comments

--- SilverSolver/assignment4/islands.py
+++ SilverSolver/assignment4/islands.py
@@ -1,28 +1,20 @@
-from collections import deque
+from copy import deepcopy
 from typing import List
 
 def neighbourhood(i: int, j: int, islandMatrix: List[List[int]]) -> List[List[int]]:
-    # helper function, returns the neighbourhood of (i, j) cell in islandMatrix
-    neigh = []
-    if i > 0:
-        if islandMatrix[i - 1][j] == 1:
-            neigh.append((i - 1, j))
-    if i < len(islandMatrix) - 1:
-        if islandMatrix[i + 1][j] == 1:
-            neigh.append((i + 1, j))
-    if j > 0:
-        if islandMatrix[i][j - 1] == 1:
-            neigh.append((i, j - 1))
-    if j < len(islandMatrix[0]) - 1:
-        if islandMatrix[i][j + 1] == 1:
-            neigh.append((i, j + 1))
-    return neigh
+    "helper function, returns the neighbourhood of (i, j) cell in islandMatrix"
+
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        ni, nj = di + i, dj + j # ni, nj are a neighbor candidates for i, j
+        if 0 <= ni < len(islandMatrix) and 0 <= nj < len(islandMatrix[0]) and islandMatrix[ni][nj]:
+            yield ni, nj
 
 def island_fill(i: int, j: int, islandMatrix: List[List[int]]) -> List[List[int]]:
-    # helper function, fills the island, to which (i, j) belongs, with "2"
-    # it will help to distinct it with the islands in which we've never been
-    q = deque()
-    q.append((i, j))
+    """
+    helper function, fills the island, to which (i, j) belongs, with "2"
+    it will help to distinct it with the islands in which we've never been
+    """
+    q = [(i, j)]
     while q:
         element = q.pop()
         if islandMatrix[element[0]][element[1]] == 1:
@@ -32,13 +24,13 @@ def island_fill(i: int, j: int, islandMatrix: List[List[int]]) -> List[List[int]
     return islandMatrix
 
 def get_number_of_islands(islandMatrix: List[List[int]]) -> int:
-    # the essential function
+    "the essential function"
+    
+    islandMatrix_r = deepcopy(islandMatrix)
     islandNum = 0
-    for i in range(len(islandMatrix)):
-        for j in range(len(islandMatrix[0])):
-            if islandMatrix[i][j] != 1:
-                pass
-            else:
+    for i in range(len(islandMatrix_r)):
+        for j in range(len(islandMatrix_r[0])):
+            if islandMatrix_r[i][j] == 1:
                 islandNum += 1
-                islandMatrix = island_fill(i, j, islandMatrix)
+                islandMatrix_r = island_fill(i, j, islandMatrix_r)
     return islandNum
