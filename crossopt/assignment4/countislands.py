5ab98d36a19ad8d959838fb6e01b235e6fe6d89e
crossopt <s17b2_fedorkina@179.ru>
2018-07-04 21:00:38 +0300

    some simplifications

--- crossopt/assignment4/countislands.py
+++ crossopt/assignment4/countislands.py
@@ -1,53 +1,43 @@
 #!/usr/bin/env python3
 
 
-# disjoint set union for grid components
-class _GridDSU:
-    def __init__(self, n, m):
-        self.parent = dict()
-        self.size = dict()
-        for i in range(n):
-            for j in range(m):
-                self.parent[(i, j)] = (i, j)
-                self.size[(i, j)] = 1
-
-    def __getitem__(self, u):
-        if self.parent[u] != u:
-            self.parent[u] = self[self.parent[u]]
-        return self.parent[u]
-
-    def join(self, u, v):
-        u = self[u]
-        v = self[v]
-        if self.size[u] < self.size[v]:
-            u, v = v, u
-        self.size[u] += self.size[v]
-        self.parent[v] = u
-
-
+"""grid of tiles. stores islands via dsu"""
+"""https://en.wikipedia.org/wiki/Disjoint-set_data_structure"""
 class Grid(object):
     def __init__(self, n, m, grid):
         self.n = n
         self.m = m
         self.grid = grid
-
-    # returns DSU for grid where all islands are components
-    def colorIslands(self):
-        dsu = _GridDSU(self.n, self.m)
-        for i in range(self.n):
-            for j in range(self.m):
+        self.parents = dict()
+        self.sizes = dict()
+        for i in range(n):
+            for j in range(m):
+                self.parents[i, j] = (i, j)
+                self.sizes[i, j] = 1
                 if i and self.grid[i][j] and self.grid[i - 1][j]:
-                    dsu.join((i, j), ((i - 1, j)))
+                    self.join((i, j), ((i - 1, j)))
                 if j and self.grid[i][j] and self.grid[i][j - 1]:
-                    dsu.join((i, j), ((i, j - 1)))
-        return dsu
+                    self.join((i, j), ((i, j - 1)))
+
+    # returns unique id for each island
+    def __getitem__(self, u):
+        if self.parents[u] != u:
+            self.parents[u] = self[self.parents[u]]
+        return self.parents[u]
+    
+    def join(self, u, v):
+        u = self[u]
+        v = self[v]
+        if self.sizes[u] < self.sizes[v]:
+            u, v = v, u
+        self.sizes[u] += self.sizes[v]
+        self.parents[v] = u
 
     # returns amount of islands in grid
     def countIslands(self):
-        color = self.colorIslands()
         colors = set()
         for i in range(self.n):
             for j in range(self.m):
                 if self.grid[i][j]:
-                    colors.add(color[(i, j)])
+                    colors.add(self[(i, j)])
         return len(colors)
