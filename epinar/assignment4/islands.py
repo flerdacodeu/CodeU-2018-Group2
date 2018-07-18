6f7774807068b23d53ef5e0569b3acf36248fc36
epinar <evin.oernek@tum.de>
2018-07-18 19:27:51 +0200

    Revert "alternative graph and followup1"
    This reverts commit 0ca361bb8bdd55eb19fbcd5ae1597eb9b927e574.

--- epinar/assignment4/islands.py
+++ epinar/assignment4/islands.py
@@ -1,12 +1,11 @@
 import unittest
-import string
 
 
 def visit_island(grid, k, l, M, N):
 	""" Marks the visited island pieces to not to visit in the future again."""
 	if 0 <= k < M and 0 <= l < N:
-		if (k, l) in grid:
-			grid.remove((k, l))
+		if grid[k][l] is True:
+			grid[k][l] = False
 			visit_island(grid, k + 1, l, M, N)
 			visit_island(grid, k - 1, l, M, N)
 			visit_island(grid, k, l - 1, M, N)
@@ -16,55 +15,34 @@ def visit_island(grid, k, l, M, N):
 
 def count_islands(grid):
 	""" Counts the number of islands """
-	# visited = grid.copy()  # copy the grid in order not to lose the real information.
-	unvisited = set()
+	visited = grid.copy()  # copy the grid in order not to lose the real information.
 	M = len(grid)
 	N = len(grid[0])
-	print(grid[0][0])
-	for k in range(M):
-		for l in range(N):
-			if grid[k][l] == 'T':
-				print("found!")
-				set.add((k, l))
-
 	c = 0
 	for k in range(M):
 		for l in range(N):
-			if (k, l) in unvisited:
+			if visited[k][l]:
 				c += 1  # found a new island
-				visit_island(unvisited, k, l, M, N)  # visit the connected pieces
-				print("islands:")
+				visit_island(visited, k, l, M, N)  # visit the connected pieces
 	return c
-
-def main():
-	map1 = ["FTFT",
-					 "TTFF",
-					 'FFTF',
-					 'FFTF']
-
-	print( count_islands(map1))
-
-
-if __name__ == '__main__':
-	main()
 
 
 class CountIslands(unittest.TestCase):
 
 	def _setUp(self):
-		self.map1 = ["FTFT",
-					 "TTFF",
-					 'FFTF',
-					 'FFTF']
-		self.map2 = ['F']
-		self.map3 = ['T']
-		self.map4 = ['TT']
-		self.map5 = ['TF']
-		self.map6 = ['TF',
-					 'FT']
-		self.map7 = ['TFTFT',
-					 'FTFTF',
-					 'TFTFT']
+		self.map1 = [[False, True, False, True],
+					 [True, True, False, False],
+					 [False, False, True, False],
+					 [False, False, True, False]]
+		self.map2 = [[False]]
+		self.map3 = [[True]]
+		self.map4 = [[True, True]]
+		self.map5 = [[True, False]]
+		self.map6 = [[True, False],
+					 [False, True]]
+		self.map7 = [[True, False, True, False, True],
+					 [False, True, False, True, False],
+					 [True, False, True, False, True]]
 
 	def test_count(self):
 		self._setUp()
@@ -75,3 +53,7 @@ class CountIslands(unittest.TestCase):
 		self.assertEqual(1, count_islands(self.map5))
 		self.assertEqual(2, count_islands(self.map6))
 		self.assertEqual(8, count_islands(self.map7))
+<<<<<<< HEAD
+=======
+
+>>>>>>> e64b7cc7c439cfff7ff7a676ad777ab65a178bc2
